bf1872d95334e3b6e46286b3f1876cdc7d37e6b6
benwalker1998 <ben-walker@outlook.com>
2022-01-02 14:45:23 +0000

    Week 4 updates

--- File_Functions.py
+++ File_Functions.py
@@ -1,28 +1,25 @@
-def read_file(file_name, list_name):
-    try: #reads a file to a list
-        with open(file_name, 'r') as my_file:
-            for line in my_file.readlines():
-                list_name.append(line.rstrip())
-    except FileNotFoundError as f:
-        print("File not found!")
-    except Exception as e:
-        print(f"An error has occurred!!! {e}")
+import csv
+
+def read_csv_file(file_name, list_name):
+    with open(file_name, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            list_name.append(row)
 
 
-def write_file(file_name, list_name):
-    try: #writes list to a file 
-        with open(file_name, "w") as my_file:
-            for product in list_name:
-                my_file.writelines(product + "\n")
-    except FileNotFoundError as f:
-        print("File not found!")
-    except Exception as e:
-        print(f"An error occurred!!! {e}")
+
+def write_csv_file(file_name, list_name):
+    fieldnames = list_name[0].keys()
+    with open(file_name, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames)
+        writer.writeheader()
+        writer.writerows(list_name)
 
 
 def print_list(list_name):
+    print("\n")
     for lines in range(len(list_name)):
-         print("\n" + list_name[lines])
+         print(list_name[lines])
 
 
 def add_item(list_name):
@@ -38,18 +35,35 @@ def add_item(list_name):
         except Exception as e:
             print("An error occured!!!")
         break
+
+def add_to_dict(dict_name, list_name):
+    while True:
+        try:
+            fieldnames = list_name[0].keys()
+            name = str(input("Press Enter to exit\n<<< ")).title()
+            if name == "":
+                break
+            price = int(input("<<< "))
+            if price == "":
+                break
+            dict_name = {"Name":name, "Price": price}
+            list_name.append(dict_name)
+            break
+        except Exception as e:
+            print(f"An error occured!! {e}")
+            break
+
     
 
-def edit_list(list_name):
+def edit_list(list_name, dict_name):
     while True:
         try:
             print(list(enumerate(list_name)))
             index = int(input("Index:<<< "))
-            item = input("Edit:<<< ").title()
-            list_name[index] = item
+            list_name[index] = dict_name
             break
         except IndexError as e:
-            print("Product number does not exist!!!")
+            print("Product price does not exist!!!")
             break
         except ValueError as e:
             print("Invalid input!!!")
@@ -60,12 +74,14 @@ def del_list_element(list_name):
     while True:
         try:
             print(list(enumerate(list_name)))
-            del_input = int(input("Please enter index number to remove: "))
+            del_input = int(input("Please enter index to remove: "))
             del list_name[del_input]
             break
         except IndexError as i:
-            print("Invalid index number!!!")
+            print("Invalid index price!!!")
             break
         except ValueError as e:
             print("Please enter a valid input!!!")
             break
+
+
--- mini_project.py
+++ mini_project.py
@@ -1,11 +1,17 @@
-import File_Functions
+import File_Functions 
 
 products_list = [] #function that imports products file into list and strips whitespace
-File_Functions.read_file("products.txt", products_list)
-    
-courier_list = [] #function that imports couriers file into list and strips whitespace
-File_Functions.read_file("couriers.txt", courier_list)
+File_Functions.read_csv_file("products.csv", products_list)
+product_dict = {"Product": "None",
+"Price": "0"}
 
+courier_list = [] #function that imports couriers file into list and strips whitespace
+File_Functions.read_csv_file("couriers.csv", courier_list)
+courier_dict = {"Name": "None",
+"Phone Number": "00000000000"}
+
+order_list = []
+File_Functions.read_csv_file("Orders.csv", order_list)
 order_dict = {"Customer Name": "None",
 "Customer Address": "None",
 "Customer Phone Number": "0000000",
@@ -13,14 +19,14 @@ order_dict = {"Customer Name": "None",
 "Order Status": "Processing"}
 
 Order_status = ["Processing", "Preparing", "Shipping", "Delivered"]
-order_list = []
     
 while True: # While loop that will display the main menu
     try:
         main_menu = int(input("\nMain Menu\n3 - Order_Menu\n2 - Courier_Menu\n1 - Products_Menu\no - Exit\n<<< ")) #Main menu
         if main_menu == 0: # closes the system if user enters the value 0
-            File_Functions.write_file("products.txt", products_list) # function that writes list to file then closes file
-            File_Functions.write_file("couriers.txt", courier_list) # function that writes list to file then closes file
+            File_Functions.write_csv_file("products.csv", products_list) # function that writes list to file then closes file
+            File_Functions.write_csv_file("couriers.csv", courier_list) # function that writes list to file then closes file
+            File_Functions.write_csv_file("Orders.csv", order_list)
             break # breaks loop and ends program
         elif main_menu == 1: # moves on to the options menu if user enters the value 1
             while True: # while loop that displays the options menu
@@ -34,11 +40,48 @@ while True: # While loop that will display the main menu
                         File_Functions.print_list(products_list)
 
                     elif products_menu == 2: # while loop that allows user to add product
-                        File_Functions.add_item(products_list)
+                        while True:    
+                            try:
+                                product = input("Please enter product name: ").title()
+                                if product == "":
+                                    print("Invalid product name")
+                                    break
 
-                    elif products_menu == 3: # while loop that allows user to edit list via index 
-                        File_Functions.edit_list(products_list)
-                        
+                                price = float(input("Please enter the products price: "))
+                                if type(price) is not float:
+                                    print("Invalid input!")
+                                    break
+                                float_price = "{:.2f}".format(price)
+                                product_dict = {"Product": product,
+                                "Price": float_price}
+
+                                products_list.append(product_dict)
+                                break
+                            except Exception as e:
+                                break
+
+                    elif products_menu == 3: # while loop that allows user to edit list via index
+                        while True:    
+                            try:
+                                product = input("Please enter product name: ").title()
+                                if product == "":
+                                    print("Invalid product name")
+                                    break
+
+                                price = float(input("Please enter the products price: "))
+                                if type(price) is not float:
+                                    print("Invalid input!")
+                                    break
+                                float_price = "{:.2f}".format(price)
+                                product_dict = {"Product": product,
+                                "Price": float_price}
+
+                                File_Functions.edit_list(products_list, product_dict)
+                                break
+                            except Exception:
+                                print("An error occurred!!")
+                                break
+
                     elif products_menu == 4: # while loop that allows user to delete product
                         File_Functions.del_list_element(products_list)
                     else:
@@ -58,10 +101,32 @@ while True: # While loop that will display the main menu
                         File_Functions.print_list(courier_list)
                     
                     elif courier_menu == 2: # add to courier list
-                        File_Functions.add_item(courier_list)
+                        while True:   
+                            try:
+                                courier_name = input("Please enter courier name: ").title()
+                                if courier_name == "":
+                                    print("Invalid name")
+                                    break
+                                phone_number = input(int("Please enter the couriers phone number: "))
+                                if len(phone_number) is not 11:
+                                    print("Invalid phone number")
+                                    break
+                                courier_dict = {"Name": courier_name,
+                                "Phone Number": phone_number}
+
+                                courier_list.append(courier_dict)
+                            except Exception as e:
+                                break
+                            break
                     
                     elif courier_menu == 3: # while loop that allows user to edit list via index 
-                        File_Functions.edit_list(courier_list)
+                        courier_name = input("Please enter courier name: ").title()
+                        phone_number = input("Please enter the couriers phone number: ")
+
+                        courier_dict = {"Name": courier_name,
+                        "Phone Number": phone_number}
+
+                        File_Functions.edit_list(courier_list, courier_dict)
                     
                     elif courier_menu == 4: # while loop that allows user to delete courier
                         File_Functions.del_list_element(courier_list)
@@ -78,12 +143,12 @@ while True: # While loop that will display the main menu
                     if order_menu == 0:
                         break
                     elif order_menu == 1:
-                        print(order_dict)
+                        File_Functions.print_list(order_list)
                     elif order_menu == 2:
-                        Customer_name = input("Please enter your name: ")
+                        Customer_name = input("Please enter your name: ").title()
                         order_dict["Customer Name"] = Customer_name
 
-                        Customer_address = input("Please enter your Address: ")
+                        Customer_address = input("Please enter your Address: ").title()
                         order_dict["Customer Address"] = Customer_address
 
                         Customer_phone_number = int(input("Please enter your phone number: "))
@@ -105,24 +170,25 @@ while True: # While loop that will display the main menu
                     elif order_menu == 4:
                         print(list(enumerate(order_list)))
                         order_list_index = int(input("Please select order via index: "))
-                        Customer_name = input("Please enter your name: ")
+
+                        Customer_name = input("Please enter your name: ").title()
                         if Customer_name == "":
                             print("Invalid input!!")
-                            print(order_menu)
+                            break
                         else:
                             order_list[order_list_index]["Customer Name"] = Customer_name
 
-                        Customer_address = input("Please enter your Address: ")
+                        Customer_address = input("Please enter your Address: ").title()
                         if Customer_address == "":
                             print("Invalid input!!")
-                            print(order_menu)
+                            break
                         else:
                             order_list[order_list_index]["Customer Address"] = Customer_address
 
                         Customer_phone_number = int(input("Please enter your phone number: "))
                         if Customer_phone_number == "":
                             print("Invalid input!!")
-                            print(order_menu)
+                            break
                         else:
                             order_list[order_list_index]["Customer Phone Number"] = Customer_phone_number
 
@@ -146,7 +212,7 @@ while True: # While loop that will display the main menu
         print("Please enter a valid number!")
     
     except Exception as e:
-        print("An error has occurred!!!")
+        print(f"An error has occurred!!!\n{e}")
         break
 
 print("System Exit!!!")
